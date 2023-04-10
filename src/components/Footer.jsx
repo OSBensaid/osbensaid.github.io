fcf32d377c440afa54f817349d8b997cfba60ced
Oussama Bensaid <osbensaid@gmail.com>
2023-04-10 10:52:54 -0400

    firt commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import "../styles/Footer.css";
+function Footer() {
+  return (
+    <div className="footer">
+      <p>&copy; 2022 Oussama Bensaid</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/helpers/experience.js
@@ -0,0 +1,40 @@
+const expriences = [
+  {
+    id: 3,
+    title: "Certification - AWS Cloud Practitioner Essentials",
+    location: "Myrtle Beach, SC",
+    description:
+      "The AWS Certified Cloud Practitioner is an IT industry certification offered by Amazon. It's a certification that covers so many different AWS Cloud-related material, such as understanding essential cloud concepts and AWS services, security, architecture, pricing, and support.",
+    date: "September 2022 - Present",
+    icon: "school",
+  },
+  {
+    id: 4,
+    title: "RadixBay - Full Stack Web Developer",
+    location: "Tybor City, NC",
+    description:
+      "RadixBay is Consulting and Managed Services with offices in Charlotte and Tabor City, NC. It is a model that combines the quality and security. Including application design, development, testing, cloud data infrastructure support, and project management.",
+    date: "Jun 2021 - Present",
+    icon: "work",
+  },
+  {
+    id: 5,
+    title: "AAS in Computer Technology - Programming",
+    location: "Myrtle Beach, SC",
+    description:
+      "Associate degree in Computer Technology - Programming, in Horry-Georgetown Technical College prepares students for entry-level positions in a vast number of industries. This degree provides skills in a variety of programming languages, such as Python, Java, JavaScript, HTML, CSS, Mobile Apps, and SQL.",
+    date: "2018 - 2021",
+    icon: "school",
+  },
+  {
+    id: 6,
+    title: "GED - General Educational Development",
+    location: "Myrtle Beach, SC",
+    description:
+      "Myrtle Beach Family Learning Center is located in Myrtle Beach, SC. This school offers training, for General Educational Development (GED) Certification. I completed this education training ranges from 5 months to 6 months. The most commonly reported benefits of attending Myrtle Beach Family Learning Center Unit are Flexible class hours and a Positive environment.",
+    date: "2016 - 2017",
+    icon: "school",
+  },
+];
+
+export default expriences;
